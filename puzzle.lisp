cec5e6cc14808ac0eec11911dd7206c97e2c4039
202100067LucasAlmeida <202100067@estudantes.ips.pt>
2025-12-10 11:05:53 +0000

    bla bla bla

--- puzzle.lisp
+++ puzzle.lisp
@@ -13,6 +13,9 @@
 # > Nota: Funções assinaladas com (*) são funções definidas além do enunciado.
 |#
 
+
+;; Variáveis de teste e operadores
+
 ;;; Tabuleiros
 
 (defun tabuleiro-teste ()
@@ -26,6 +29,11 @@
 	 (nil nil 1 1 1 nil nil)
 	 (nil nil 1 1 1 nil nil)
 	)
+)
+
+(defun no-teste ()
+  "Cria um nó para testes"
+  (list (tabuleiro-teste) 0 nil)
 )
 
 ;; Seletores
@@ -93,6 +101,12 @@
 
 ;; Operadores
 
+;; Operadores
+(defun operadores ()
+  "Cria uma lista com todos os operadores do jogo solitário"
+  (list 'operador-cd 'operador-ce 'operador-cc 'operador-cb)
+)
+
 ;; Captura Direita
 (defun operador-cd (x y tabuleiro)
   "Realizar uma captura de pino à direita"
@@ -138,5 +152,76 @@
              ((equal (celula (1+ x) y tabuleiro) 0) nil)
              ((equal (celula (+ x 2) y tabuleiro) 1) nil)
              (t (substituir x y (substituir (1+ x) y (substituir (+ x 2) y tabuleiro 1) 0) 0))
+   )
+)
+
+;; Nós
+
+;; Construtor
+(defun cria-no (tabuleiro &optional (p 0) (pai nil))
+  "Criar um nó com o estado do tabuleiro sua profundidade e seu nó pai"
+  (list tabuleiro p pai)
+)
+
+;; Seletores
+;; No-estado
+(defun no-estado (no)
+  "Ver o estado do tabuleiro"
+  (first no)
+)
+
+;; No-profundidade
+(defun no-profundidade (no)
+  "Ver a profundidade do nó"
+  (second no)
+)
+
+;; No-pai
+(defun no-pai (no)
+  "Ver o nó pai do nó"
+  (third no)
+)
+
+;; No-solucao
+(defun no-solucaop (no)
+  "Verifica se o nó é solução, se sim devolve o nó"
+  (cond ((some #'(lambda (x) (= x 1)) (no-estado no)) no)
+        (t nil)
+  )
+)
+
+;; No-existep
+(defun no-existep (no nos)
+  (cond ((or (null no) (null nos)) nil)
+             ((not (listp nos)) nil)
+             ((equal no (first nos)) t)
+             (t (no-existep no (rest nos)))
+   )
+)
+
+
+;; Sucessores
+
+;; novo-sucessor
+(defun novo-sucessor (no operador)
+  "Cria um novo sucessor do no aplicado o operador"
+  (let ((novo-estado (and no operador
+                          (eval (list operador (list 'quote (no-estado no)))))))
+    (cond
+      ((null no) nil)
+      ((null operador) nil)
+      ((null novo-estado) nil)
+      (t (cria-no novo-estado (+ (no-profundidade no) 1) no)))
+  )
+)
+
+;; Sucessores
+(defun sucessores (no operadores procura &optional (profundidade))
+  "Gera a lista de sucessores no no"
+  (cond ((null no) nil)
+             ((null operadores) nil)
+             ((null procura) nil)
+             ((and (equal procura 'dfs) (= (no-profundidade no) profundidade)) nil)
+             (t (mapcar #'(lambda (x) (novo-sucessor no x)) operadores))
    )
 )
